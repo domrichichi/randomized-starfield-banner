5eeb854f2c601530b7bc3c8c2f1f9db04fa68190
domrichichi <domrichichi@gmail.com>
2016-04-05 11:59:03 -0400

    fixing js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var $banner = $('.banner');
-var $addStars;
-var $width = $banner.outerWidth();
-var $height = $banner.outerHeight();
+var $body = $('.banner');
+var $star;
+var width = $body.outerWidth();
+var height = $body.outerHeight();
 
 for (i = 0; i < 75; i++) {
   $rotateNum = Math.random() * 1000;
