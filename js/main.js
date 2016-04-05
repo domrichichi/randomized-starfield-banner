ab8ac2a8745c607f384ffd2d6d980ebcd1fcce68
domrichichi <domrichichi@gmail.com>
2016-04-05 11:17:56 -0400

    finalized for statement

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ var $height = $banner.outerHeight();
 for (i = 0; i < 75; i++) {
   $rotateNum = Math.random() * 1000;
   $scaleNum = Math.random() * 10000;
+  $animateNum = Math.random() * 50;
+  $addStars = $('<div>');
+  $addStars.addClass('star');
+  $addStars.css({
+    left: Math.random() * 1000,
+    top: Math.random() * 1000,
+    opacity: Math.random() * 10,
+    width: Math.random() * 100,
+    transform: 'rotate(' + $rotateNum + 'deg)',
+    transform: 'scale(' + $scaleNum + '%)',
+    animation: 'growstars ' + $animateNum + 's alternate infinite'
+  });
