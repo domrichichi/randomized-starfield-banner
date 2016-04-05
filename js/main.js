3824401cc2caef453056eda7ec0238f34dedb6d6
domrichichi <domrichichi@gmail.com>
2016-04-05 11:16:04 -0400

    adding for statements

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ var $banner = $('.banner');
 var $addStars;
 var $width = $banner.outerWidth();
 var $height = $banner.outerHeight();
+
+for (i = 0; i < 75; i++) {
+  $rotateNum = Math.random() * 1000;
+  $scaleNum = Math.random() * 10000;
